a9ec8142ba3f9275c56cbe1c75b04a53ae0e7ffd
be <beoppchaser@gmail.com>
2024-04-16 00:05:14 +0800

    implement basic function of clipboard-push

--- src/http/clipboard-pull.ts
+++ src/http/clipboard-pull.ts
@@ -1,8 +1,23 @@
 import { Express } from "express";
 import fs from "fs";
+import { LocalStorage } from "node-localstorage";
 
 module.exports = (app: Express) => app.get('/clipboard-pull', async (req, res) => {
-    const clipboard_data = fs.readFileSync('clipboard', 'utf8');
-    res.send(clipboard_data);
-    console.log('send: ' + clipboard_data);
+    try {
+        // select the correct file type and send content
+        const extensionName = new LocalStorage('type').getItem('type');
+
+        if (extensionName === 'txt') {
+            const clipboard_data = fs.readFileSync('clipboard.txt', 'utf8');
+            res.send(clipboard_data);
+        } else if (extensionName === 'png') {
+            res.sendFile('clipboard.png', {root: __dirname + '/..'});
+        } else {
+            throw new Error('Unsupported file type');
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).send(error);
+        return;
+    }
 });
--- src/http/clipboard-push.ts
+++ src/http/clipboard-push.ts
@@ -25,36 +25,44 @@ module.exports = (app: Express) => app.post('/clipboard-push', checkSchema({
 
     // parse data for different platforms
     if (platform === 'ios') {
-        // decode and extract data file
-        fs.writeFileSync('clipboard.zip', data, 'base64');
-        const zip = new Zip('clipboard.zip');
-        const entryName = zip.getEntries()[0].entryName;
-        const extensionName = entryName.split('.').pop();
-        zip.extractAllTo('.', true);
+        try {
+            // decode and extract data file
+            fs.writeFileSync('clipboard.zip', data, 'base64');
+            const zip = new Zip('clipboard.zip');
+            const entryName = zip.getEntries()[0].entryName;
+            const extensionName = entryName.split('.').pop();
+            zip.extractAllTo('.', true);
 
-        // parse data for different file types
-        if (extensionName === 'rtf') {
-            new LocalStorage('type').setItem('type', 'txt');
-            // need catdoc CLI tool installed
-            exec(`catdoc -d unicode ${entryName}`, (error, stdout, stderr) => {
-                if (error) {
-                    console.error(`exec error: ${error}`);
-                    return;
-                }
-                fs.writeFileSync('clipboard.txt', stdout);
-            });
-        } else if (extensionName === 'txt') {
-            new LocalStorage('type').setItem('type', 'txt');
-        } else if (extensionName === 'png') {
-            new LocalStorage('type').setItem('type', 'png');
-        } else if (extensionName === 'heic') {
-            const inputBuffer = fs.readFileSync(entryName);
-            const outputBuffer = await convert({
-                buffer: inputBuffer,
-                format: 'PNG'
-            });
-            fs.writeFileSync('clipboard.png', Buffer.from(outputBuffer));
+            // parse data for different file types
+            if (extensionName === 'rtf') {
+                new LocalStorage('type').setItem('type', 'txt');
+                // need catdoc CLI tool installed
+                exec(`catdoc -d unicode ${entryName}`, (error, stdout, stderr) => {
+                    if (error) {
+                        throw new Error('Conversion failed');
+                    }
+                    fs.writeFileSync('clipboard.txt', stdout.trim(), 'utf8');
+                });
+            } else if (extensionName === 'txt') {
+                new LocalStorage('type').setItem('type', 'txt');
+            } else if (extensionName === 'png') {
+                new LocalStorage('type').setItem('type', 'png');
+            } else if (extensionName === 'heic') {
+                new LocalStorage('type').setItem('type', 'png');
+                const inputBuffer = fs.readFileSync(entryName);
+                const outputBuffer = await convert({
+                    buffer: inputBuffer,
+                    format: 'PNG'
+                });
+                fs.writeFileSync('clipboard.png', Buffer.from(outputBuffer));
+            } else {
+                throw new Error('Unsupported file type');
+            }
+        } catch (error) {
+            console.log(error);
+            res.status(500).send(error);
+            return;
         }
     }
-    res.send('Clipboard updated!');
+    res.send('Clipboard updated');
 });
